6d00bad447d8ab648875b57191a4896a032f6746
Glitch (puppeteer-api) <none>
2020-05-12 19:17:11 +0000

    ⭐️💠 Updated with Glitch

--- public/script.js
+++ public/script.js
@@ -36,15 +36,42 @@ document.querySelector("iframe").onload = function() {
 
 goButton.addEventListener('click', async function() {
   
+  
    var url = omnibox.value;
   document.querySelector("#loader").classList = ""
   
   console.log("The url to convert is", url)
   
+  
+  if (!url.includes('http://') && !url.includes('https://')) {
+    alert("missing protocol");
+    
+  const requestConfig = {
+    method: "GET",
+        headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    }
+
+  };
+  // let request = await fetch(`https://${url}`, requestConfig);
+      let request = await fetch(`https://www.google.com`, requestConfig);
+
+  const responses = await request.json();
+  console.log(responses);
+
+    // fetch("https://" + url)
+    // .then(response => console.log(response))
+    // .then(contents => console.log(contents))
+    // .catch(error => console.log(error.message));
+
+    
+  }
+   
+  
 
     try {
       const encoded = encodeURIComponent(url);
-      
       const structuredUrl = `/pdf?url=${encoded}`
       // const data = await fetch(`/pdf?url=${encoded}`);
       
@@ -57,6 +84,6 @@ goButton.addEventListener('click', async function() {
     } catch (err) {
       console.log(err)
     }
-
+  
   
 });
